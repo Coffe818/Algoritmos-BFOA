f079dcf5a54e5d4273202418f22b112375d65b6c
Your Name <you@example.com>
2024-10-05 10:34:45 -0600

    Cambios para que genere los CSV

--- BactObs/BFOA_MSAv2.py
+++ BactObs/BFOA_MSAv2.py
@@ -1,20 +1,23 @@
-from bacteria import bacteria
+# from bacteria import bacteria
 from chemiotaxis import chemiotaxis
 
 import numpy
+import csv
 
-poblacion = []
-path = "C:\secuenciasBFOA\multiFasta.fasta"
+#asegruarse que esten aen la ruta correcta, si no funciona conesta ruta prueba con la ruta absoluta
+pathA = "Algoritmos\Secuencias\SetA.fasta"
+pathB = "Algoritmos\Secuencias\SetB.fasta"
+pathC = "Algoritmos\Secuencias\SetC.fasta"
+paths = [pathA, pathB, pathC]
 numeroDeBacterias = 5
 numRandomBacteria = 1
 iteraciones = 30
 tumbo = 1                                              #numero de gaps a insertar 
 nado = 3
 chemio = chemiotaxis()
-veryBest = bacteria(path)                #mejor bacteria   
-tempBacteria = bacteria(path)            #bacteria temporal para validaciones
-original = bacteria(path)                #bacteria original sin gaps
-globalNFE = 0      #numero de evaluaciones de la funcion objetivo
+
+
+
 
 dAttr= 0.1 #0.1
 wAttr= 0.2 #0.2
@@ -43,36 +46,58 @@ def validaSecuencias(path, veryBest):
             return
       
         
+
+for ejecucion in range(30):
+    print(f"\n--- Ejecución {ejecucion + 1} ---\n")
     
-    
-    
-    
-    
-    
-    
+    numsec=0
+
+    #recorrer cada secuencia
+    for path in paths:
+        from bacteria import bacteria
+
+        numsec+=1
+        print(f"\n--- Archivo {path} ---\n")
+        veryBest = bacteria(path)                #mejor bacteria   
+        tempBacteria = bacteria(path)            #bacteria temporal para validaciones
+        original = bacteria(path)                #bacteria original sin gaps
+        globalNFE = 0      #numero de evaluaciones de la funcion objetivo
+        
+        poblacion = []                                             #poblacion inicial
+
+        for i in range(numeroDeBacterias): 
+            poblacion.append(bacteria(path))
 
 
-for i in range(numeroDeBacterias):                                            #poblacion inicial
-    poblacion.append(bacteria(path))
+        for _ in range(iteraciones):                                                  #numero de iteraciones  
+            for bacteria in poblacion:
+                bacteria.tumboNado(tumbo)
+                #bacteria.tumboNado(nado)
+                bacteria.autoEvalua()  
+                #print("blosumScore: ",bacteria.blosumScore)
+            chemio.doChemioTaxis(poblacion, dAttr, wAttr, hRep, wRep)                 #d_attr, w_attr, h_rep, w_rep):
+            globalNFE += chemio.parcialNFE 
+            best = max(poblacion, key=lambda x: x.fitness)
+            if (veryBest == None) or (best.fitness > veryBest.fitness):
+                clonaBest(veryBest, best)
+            print("interaccion: ",veryBest.interaction,"fitness: ",veryBest.fitness, " NFE:",globalNFE )
+            
+            chemio.eliminarClonar(path, poblacion)
+            chemio.insertRamdomBacterias(path, numRandomBacteria, poblacion)                #inserta  bacterias aleatorias
+            print("poblacion: ",len(poblacion))
 
 
-for _ in range(iteraciones):                                                  #numero de iteraciones  
-    for bacteria in poblacion:
-        bacteria.tumboNado(tumbo)
-        #bacteria.tumboNado(nado)
-        bacteria.autoEvalua()  
-        #print("blosumScore: ",bacteria.blosumScore)
-    chemio.doChemioTaxis(poblacion, dAttr, wAttr, hRep, wRep)                 #d_attr, w_attr, h_rep, w_rep):
-    globalNFE += chemio.parcialNFE 
-    best = max(poblacion, key=lambda x: x.fitness)
-    if (veryBest == None) or (best.fitness > veryBest.fitness):
-         clonaBest(veryBest, best)
-    print("interaccion: ",veryBest.interaction,"fitness: ",veryBest.fitness, " NFE:",globalNFE )
-    
-    chemio.eliminarClonar(path, poblacion)
-    chemio.insertRamdomBacterias(path, numRandomBacteria, poblacion)                #inserta  bacterias aleatorias
-    print("poblacion: ",len(poblacion))
+        veryBest.showGenome()
+        validaSecuencias(path, veryBest)
 
 
-veryBest.showGenome()
-validaSecuencias(path, veryBest)
+        #Crea archgivo de resultados en csv 
+        nameFile= 'BactObsresultados'+str(numsec)+'.csv'
+
+        with open(nameFile, mode='a', newline='') as file:  # 'a' para agregar, 'w' para sobrescribir
+            writer = csv.writer(file)
+            if ejecucion == 0:
+                # Escribir la cabecera
+                writer.writerow(["Fitness", "NFE"])
+            # Escribir los datos de la última ejecución
+            writer.writerow([veryBest.fitness, globalNFE])
--- BactOp/BFOA_MSAv2.py
+++ BactOp/BFOA_MSAv2.py
@@ -1,24 +1,26 @@
 from bacteria import bacteria
 from chemiotaxis import chemiotaxis
 import numpy
+import csv
 
-poblacion = []
-path = "../BFOA/multiFasta.fasta"
+#asegruarse que esten aen la ruta correcta, si no funciona conesta ruta prueba con la ruta absoluta
+pathA = "Algoritmos\Secuencias\SetA.fasta"
+pathB = "Algoritmos\Secuencias\SetB.fasta"
+pathC = "Algoritmos\Secuencias\SetC.fasta"
+paths = [pathA, pathB, pathC]
 numeroDeBacterias = 5
 numRandomBacteria = 1
 iteraciones = 30
 tumbo = 1                                              #numero de gaps a insertar 
 nado = 3
 chemio = chemiotaxis()
-veryBest = bacteria(path)                #mejor bacteria   
-tempBacteria = bacteria(path)            #bacteria temporal para validaciones
-original = bacteria(path)                #bacteria original sin gaps
-globalNFE = 0      #numero de evaluaciones de la funcion objetivo
 
-dAttr= 0.0001 #0.1
-wAttr= 0.0002 #0.2
+
+dAttr= 0.1 #0.1
+wAttr= 0.2 #0.2
 hRep=dAttr
 wRep= 10    #10
+
 
 
 
@@ -65,27 +67,56 @@ def tumboNado_iterativo(bacteria, tumbo, pasos=5):
     return bacteria  # Retornar la bacteria mejorada
 
 
-for i in range(numeroDeBacterias):                                            #poblacion inicial
-    poblacion.append(bacteria(path))
-    #print("bacteria: ",i,poblacion[i].matrix.seqs)
+for ejecucion in range(30):
+    print(f"\n--- Ejecución {ejecucion + 1} ---\n")
+    
+    numsec=0
+
+    #recorrer cada secuencia
+    for path in paths:
+        from bacteria import bacteria
+
+        numsec+=1
+        print(f"\n--- Archivo {path} ---\n")
+        veryBest = bacteria(path)                #mejor bacteria   
+        tempBacteria = bacteria(path)            #bacteria temporal para validaciones
+        original = bacteria(path)                #bacteria original sin gaps
+        globalNFE = 0      #numero de evaluaciones de la funcion objetivo
+        
+        poblacion = [] 
+
+        for i in range(numeroDeBacterias):                                            #poblacion inicial
+            poblacion.append(bacteria(path))
+            #print("bacteria: ",i,poblacion[i].matrix.seqs)
 
 
-for _ in range(iteraciones  ):                                                  #numero de iteraciones  
-    for i, bacteria_actual in enumerate(poblacion):
-        poblacion[i] = tumboNado_iterativo(bacteria_actual, tumbo)
+        for _ in range(iteraciones  ):                                                  #numero de iteraciones  
+            for i, bacteria_actual in enumerate(poblacion):
+                poblacion[i] = tumboNado_iterativo(bacteria_actual, tumbo)
 
-        #print("blosumScore: ",bacteria.blosumScore)
-    chemio.doChemioTaxis(poblacion, dAttr, wAttr, hRep, wRep)                 #d_attr, w_attr, h_rep, w_rep):
-    globalNFE += chemio.parcialNFE 
-    best = max(poblacion, key=lambda x: x.fitness)
-    if (veryBest == None) or (best.fitness > veryBest.fitness):
-        clonaBest(veryBest, best)
-    print("interaccion: ",veryBest.interaction,"fitness: ",veryBest.fitness, " NFE:",globalNFE )
-    #veryBest.showGenome()
-    chemio.eliminarClonar(path, poblacion)
-    chemio.insertRamdomBacterias(path, numRandomBacteria, poblacion)                #inserta  bacterias aleatorias
-    print("poblacion: ",len(poblacion))
+                #print("blosumScore: ",bacteria.blosumScore)
+            chemio.doChemioTaxis(poblacion, dAttr, wAttr, hRep, wRep)                 #d_attr, w_attr, h_rep, w_rep):
+            globalNFE += chemio.parcialNFE 
+            best = max(poblacion, key=lambda x: x.fitness)
+            if (veryBest == None) or (best.fitness > veryBest.fitness):
+                clonaBest(veryBest, best)
+            print("interaccion: ",veryBest.interaction,"fitness: ",veryBest.fitness, " NFE:",globalNFE )
+            #veryBest.showGenome()
+            chemio.eliminarClonar(path, poblacion)
+            chemio.insertRamdomBacterias(path, numRandomBacteria, poblacion)                #inserta  bacterias aleatorias
+            print("poblacion: ",len(poblacion))
 
-print("Mejor bacteria: ")
-veryBest.showGenome()
-validaSecuencias(path, veryBest)
+        print("Mejor bacteria: ")
+        veryBest.showGenome()
+        validaSecuencias(path, veryBest)
+
+        #Crea archgivo de resultados en csv 
+        nameFile= 'BactOpsresultados'+str(numsec)+'.csv'
+
+        with open(nameFile, mode='a', newline='') as file:  # 'a' para agregar, 'w' para sobrescribir
+            writer = csv.writer(file)
+            if ejecucion == 0:
+                # Escribir la cabecera
+                writer.writerow(["Fitness", "NFE"])
+            # Escribir los datos de la última ejecución
+            writer.writerow([veryBest.fitness, globalNFE])
